d4e621e32c11f2856dc9eae2f934d87448bfd2a6
ruxshona <@ruxshon.abdulatipova>
2023-12-21 01:41:43 +0500

    final

---
+++ app.js
@@ -0,0 +1,41 @@
+/* var myName = prompt(' Write your name... ')
+console.log(myName)
+
+
+var age = +prompt('Write your age...')
+console.log(age)
+
+
+var task = 'you should solve the given tasks.'
+alert(task)
+
+var task1 = +prompt(' 3 + 5 = ')
+console.log( 'solution:   3 + 5 = 8   your answer is: '+ task1)
+
+
+var task2= +prompt('7 - 3 =')
+console.log('solution: 7 - 3 = 4   your answer is: '+ task2)
+
+
+var task3 = +prompt ('4 * 6 = ')
+console.log( 'solution: 4 * 6 = 24   your answer: ' + task3)
+
+
+var task4 = +prompt ( '10 / 2 = ')
+console.log( 'solution: 10 / 2 = 5   your answer ' + task4 )
+
+var task5 = prompt ( '15 / 4 = ')
+console.log( 'solution: 15 / 4 = 3 с остатком 3 your answer:' + task5) 
+
+var check = 'open console in order to check your answers...'
+alert(check) */
+
+var x = +prompt('введите любое число(1)')
+var y = +prompt('введите любое число(2)')
+var z = +prompt('введите любое число(3)')
+alert ((x + y + z)/3)
+
+
+
+
+
